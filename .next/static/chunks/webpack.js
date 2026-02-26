d504b6189f7519300b155d0cb0d696113909d4bf
detriste <detriste2018@gmail.com>
2026-02-26 16:59:28 -0300

    ecscscs

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -192,7 +192,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "63ef10ae4829aecf"; }
+/******/ 		__webpack_require__.h = function() { return "a37d5bb34dc62771"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
